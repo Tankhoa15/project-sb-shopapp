a216bb9fb5f6b94130bc7bbd3e2be076d64d3633
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-06-19 01:04:32 +0700

    upload file image

--- src/main/java/com/example/shopapp/controller/ProductController.java
+++ src/main/java/com/example/shopapp/controller/ProductController.java
@@ -2,20 +2,32 @@ package com.example.shopapp.controller;
 
 import com.example.shopapp.dto.ProductDTO;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> createProduct(
-            @Valid @RequestBody ProductDTO productDTO,
+            @Valid @ModelAttribute ProductDTO productDTO,
+            //@RequestPart("file") MultipartFile file,
             BindingResult result
             ){
         try {
@@ -26,10 +38,48 @@ public class ProductController {
                         .toList();
                 return ResponseEntity.badRequest().body(errorMessages);
             }
+            MultipartFile file = productDTO.getFile();
+            if(file != null){
+                //Check size and format of file
+                if(file.getSize() > 10 * 1024 * 1024){
+                    //throw new ResponseStatusException(HttpStatus.PAYLOAD_TOO_LARGE, "File is too large, Maximum size is 10MB");
+                    return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                            .body("File is too large, Maximum size is 10MB");
+                }
+
+                String contentType = file.getContentType();
+                if(contentType == null || !contentType.startsWith("image/")){
+                    return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                            .body("File must be an image");
+                }
+
+                // Store file
+                String fileName = storeFile(file);
+            }
+
             return ResponseEntity.ok("Product create successfully!");
         }catch (Exception e){
             return  ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    private String storeFile(MultipartFile file) throws IOException {
+        // Normalize file name
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        // Generate unique file name
+        String uniqueFileName = UUID.randomUUID().toString() + "_" + filename;
+        // Create directory to save file
+        Path uploadDir = Paths.get("uploads");
+        // if directory not exist, create it
+        if (!Files.exists(uploadDir)) {
+            Files.createDirectories(uploadDir);
+        }
+        // path to save file
+        Path destination = Paths.get(uploadDir.toString(), uniqueFileName);
+        // copy file to destination
+        Files.copy(file.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
+
+        return uniqueFileName;
     }
 
     @GetMapping
