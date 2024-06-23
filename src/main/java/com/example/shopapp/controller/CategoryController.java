335fc0026f775378eb502d886ff021980f9740a8
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-06-23 21:43:54 +0700

    api prefix

--- src/main/java/com/example/shopapp/controller/CategoryController.java
+++ src/main/java/com/example/shopapp/controller/CategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/category")
+@RequestMapping("${api.prefix}/category")
 public class CategoryController {
 
     @GetMapping
